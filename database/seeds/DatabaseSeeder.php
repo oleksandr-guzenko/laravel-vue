6218ec29e2839f38820e31b7827c0220bef6def4
DragonWarrior980706 <dragon.warrior980706@gmail.com>
2020-06-03 20:20:18 +0800

    Changes in BackEndTableSeeder Class.

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -14,12 +14,17 @@ class DatabaseSeeder extends Seeder
     {
         // $this->call(UsersTableSeeder::class);
 
+        // use this Seeder Class but need to change the Header Column Names of the Spreadsheet to corresponse to the Table Column Names
+        // the Header Column Names must be the same as the Table Column Name, case-sensitive is a must in order for this to work properly
+        $this->call(BackEndTableSeeder::class);
+
+        /*
         $this->call([
-            //SpreadsheetSeeder::class,
-            BackEndTableSeeder::class,
-            //PropertiesTableSeeder::class,
-            //AnalyticTypesTableSeeder::class,
-            //PropertyTypesTableSeeder::class,
+            SpreadsheetSeeder::class,
+            PropertiesTableSeeder::class,
+            AnalyticTypesTableSeeder::class,
+            PropertyTypesTableSeeder::class,
         ]);
+        */
     }
 }
